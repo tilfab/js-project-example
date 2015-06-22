09eba076da8e90cc38f36b28fdad19d94d80331f
Haitham Zaben <haitham@tilf.se>
2015-06-22 11:33:32 +0200

    updated karma.conf.js to autoWatch and run in PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -57,12 +57,12 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    browsers: ["PhantomJS"],
 
 
     // Continuous Integration mode
